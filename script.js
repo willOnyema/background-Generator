308c7696c8b574a6d77b567f68cbcd008dd55f00
William Onyema <williamonyema@Williams-MBP.broadband>
2023-03-31 13:18:44 +0100

    add bg generator

---
+++ script.js
@@ -0,0 +1,7 @@
+var css = document.querySelector("h3");
+var colour1 = document.querySelector(".colour1");
+var colour2 = document.querySelector(".colour2");
+
+colour1.addEventListener("input", function(){
+	console.log()
+})
