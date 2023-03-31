fb974bfe15546c985ee470c5cdc4bed0f55774a3
William Onyema <williamonyema@Williams-MBP.broadband>
2023-03-31 16:34:58 +0100

    fixed the bugs on the script

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var css = document.querySelector("h3");
 var colour1 = document.querySelector(".colour1");
 var colour2 = document.querySelector(".colour2");
+var body = document.getElementById("gradient");
 
-colour1.addEventListener("input", function(){
-	console.log()
-})
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right,"
+	+ colour1.value
+	+ ", "
+	+ colour2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+colour1.addEventListener("input", setGradient);
+
+colour2.addEventListener("input", setGradient);
